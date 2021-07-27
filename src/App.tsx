5d4c05b2f5f8861af5d087f49c8a91a3d5bc1c26
kasperweidick <kasperweidick@gmail..com>
2021-07-27 11:31:34 +0200

    End of feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import {MouseEvent, useState, useCallback} from 'react'
-// import fetchText from './components/fetchText'
+import * as d3 from 'd3'
 
-// const csvUrl = 'https://gist.githubusercontent.com/kasperweidick/31adaa4f29ba0469c3b00835fd75a624/raw/CSS_colors.csv'
+const csvUrl = 'https://gist.githubusercontent.com/kasperweidick/31adaa4f29ba0469c3b00835fd75a624/raw/CSS_colors.csv'
+
+d3.csv(csvUrl).then(data => {
+    let message = ''
+        message += Math.round(d3.csvFormat.length / 1024) + ' kb \n'
+        message += `${data.length} rows \n`
+        message += `${data.columns.length} columns \n`
+        console.log(message)
+})
+
 
 // fetchText(csvUrl).then(text => {
-//     console.log(text)
+//     const data = csvParse(text) 
+//     let message = ''
+//     message += Math.round(text.length / 1024) + ' kb \n'
+//     message += `${data.length} rows \n`
+//     message += `${data.columns.length} columns \n`
+//     console.log(message)
 // })
+
+// fetchText(csvUrl)
 
 const width = 960
 const height = 500
