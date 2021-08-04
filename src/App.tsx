d5120ad4a8183246bd389f8cf2d7120ad3708c17
kasperweidick <kasperweidick@gmail..com>
2021-08-04 20:23:30 +0200

    feat: messy react bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import { useState, useEffect } from 'react'
 import { csv, scaleBand, scaleLinear, max } from 'd3'
-import { DSVRowArray } from 'd3'
-
-// interface Data {
-//     Keyword: string
-//     Specification: string
-//     'RGB hex value': string
-// }
+// import useData from './components/useData'
 
 const width = 960
 const height = 500
 
-const margin = { top: 20, right: 20, bottom: 20, left: 20 }
+const margin = { top: 20, right: 20, bottom: 20, left: 200 }
+
+const csvUrl =
+    'https://gist.githubusercontent.com/kasperweidick/baf467a5a6680a6bf9d30b84c7234c5c/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 
 const innerHeight = height - margin.top - margin.bottom
 const innerWidth = width - margin.right - margin.bottom
 
-function App() {
+const useData = () => {
     const [data, setData] = useState<Array<any>>()
-
-    const csvUrl =
-        'https://gist.githubusercontent.com/kasperweidick/baf467a5a6680a6bf9d30b84c7234c5c/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 
     useEffect(() => {
         const row = (d: any) => {
@@ -32,6 +26,12 @@ function App() {
             console.log(data.slice(0, 10))
         })
     }, [])
+
+    return data
+}
+
+function App() {
+    const data = useData()
 
     if (!data) {
         return <h1>Loading...</h1>
@@ -47,16 +47,40 @@ function App() {
 
     return (
         <svg width={width} height={height}>
-            <g
-                transform={`translate(${margin.left}, ${margin.top})`}
-            >
-                {data.map((d, i) => (
+            <g transform={`translate(${margin.left}, ${margin.top})`}>
+                {xScale.ticks().map((tickValue: number) => (
+                    <g
+                        key={tickValue}
+                        transform={`translate(${xScale(tickValue)}, 0)`}
+                    >
+                        <line y2={innerHeight} stroke='black' />
+                        <text
+                            textAnchor='middle'
+                            y={innerHeight + 3}
+                            dy='0.71em'
+                        >
+                            {tickValue}
+                        </text>
+                    </g>
+                ))}
+                {yScale.domain().map((tickValue: number) => (
+                    <text
+                        key={tickValue}
+                        textAnchor='end'
+                        dy='0.32em'
+                        x={-3}
+                        y={yScale(tickValue) + yScale.bandwidth() / 2}
+                    >
+                        {tickValue}
+                    </text>
+                ))}
+                {data.map((d) => (
                     <rect
+                        key={d['Country']}
                         x={0}
                         y={yScale(d['Country'])}
                         width={xScale(d['Population'])}
                         height={yScale.bandwidth()}
-                        key={i}
                     />
                 ))}
             </g>
