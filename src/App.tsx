c573f8a7927a74b26852aea8099730f304580e15
kasperweidick <kasperweidick@gmail..com>
2021-08-05 22:21:13 +0200

    feat: typescript fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 // import { useState, useEffect } from 'react'
 import { scaleBand, scaleLinear, max, ScaleBand, ScaleLinear } from 'd3'
-import useData from './components/useData'
+import useData from './utility functions/useData'
 import AxisBottom from './components/AxisBottom'
 import AxisLeft from './components/AxisLeft'
+import Marks from './components/Marks'
 
 const width = 960
 const height = 500
-
 const margin = { top: 20, right: 20, bottom: 20, left: 200 }
-
 const innerHeight = height - margin.top - margin.bottom
 const innerWidth = width - margin.right - margin.bottom
 
@@ -18,30 +17,31 @@ function App() {
     if (!data) {
         return <h1>Loading...</h1>
     }
+    data.map((d) => console.log(d))
+
+    const yValue = (d: any) => d['Country']
+    const xValue = (d: any) => d['Population']
 
     const yScale: ScaleBand<string> = scaleBand()
-        .domain(data.map((d) => d['Country']))
+        .domain(data.map(yValue))
         .range([0, innerHeight])
 
     const xScale: ScaleLinear<number, number, never> = scaleLinear()
-        .domain([0, max(data, (d) => d['Population'])])
+        .domain([0, max(data, xValue)])
         .range([0, innerWidth])
-    console.log(xScale)
 
     return (
         <svg width={width} height={height}>
             <g transform={`translate(${margin.left}, ${margin.top})`}>
                 <AxisBottom xScale={xScale} innerHeight={innerHeight} />
                 <AxisLeft yScale={yScale} />
-                {data.map((d) => (
-                    <rect
-                        key={d['Country']}
-                        x={0}
-                        y={yScale(d['Country'])}
-                        width={xScale(d['Population'])}
-                        height={yScale.bandwidth()}
-                    />
-                ))}
+                <Marks
+                    data={data}
+                    xScale={xScale}
+                    yScale={yScale}
+                    xValue={xValue}
+                    yValue={yValue}
+                />
             </g>
         </svg>
     )
